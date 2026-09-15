cd58a95199f6c030d304b6b376a1f439a4948d1b
Adelani Ayofe <princeadelani27@gmail.com>
2026-09-15 04:04:53 -0700

    restructuring errorr2

--- Server/token.js
+++ Server/token.js
@@ -1,62 +1,28 @@
-// const { sign } = require("jsonwebtoken");
-
-// const CreateAccessToken = (userID, role) => {
-//     return sign(
-//         { userID, role },
-//         process.env.ACCESS_TOKEN_SECRET,
-//         {
-//             expiresIn: "25m",
-//         }
-//     );
-// };
-
-// const CreateRefreshToken = (userID, role) => {
-//     return sign(
-//         { userID, role },
-//         process.env.REFRESH_TOKEN_SECRET,
-//         {
-//             expiresIn: "7d",
-//         }
-//     );
-// };
-
-// module.exports = {
-//     CreateAccessToken,
-//     CreateRefreshToken,
-// };
-
-
-
-const { jwt } = require("jsonwebtoken");
+const { sign } = require("jsonwebtoken");
 
 const CreateAccessToken = (userID, role) => {
-
-    return jwt.sign(
+    return sign(
         { userID, role },
         process.env.ACCESS_TOKEN_SECRET,
         {
-            expiresIn: "15m",
+            expiresIn: "25m",
         }
     );
-
 };
 
-
-
 const CreateRefreshToken = (userID, role) => {
-
-    return jwt.sign(
+    return sign(
         { userID, role },
         process.env.REFRESH_TOKEN_SECRET,
         {
             expiresIn: "7d",
         }
     );
-
 };
-
 
 module.exports = {
     CreateAccessToken,
     CreateRefreshToken,
 };
+
+
